c9a0dfad993c8dac2a1a7f03260b37e3b39c1dea
salil_mulay <salil_mulay@persistent.com>
2015-12-08 12:02:01 +0530

    MVN

---
+++ sample-json/src/main/java/com/persistent/sample_json/App.java
@@ -0,0 +1,13 @@
+package com.persistent.sample_json;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
